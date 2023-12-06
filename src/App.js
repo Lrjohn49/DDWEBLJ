a1fce9385814208342587ea7731d554116d4b1b8
Luke <Lrjohn49@gmail.com>
2023-12-05 17:58:26 -0700

    comit push

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
-import { Nav } from './components/Nav';
-import { About } from './components/About';
-import { MoreAbout } from './components/MoreAbout';
-import { Skills } from './components/Skills';
-import { Projects } from './components/Projects';
-import { ContactMe } from './components/ContactMe';
-import { Socials } from './components/Socials';
+
 
 function App() {
   return (
     <div className="App">
-      <Nav title="Logan Ankarberg"></Nav>
-      <About image_path="images/hero.jpg"></About>
-      <MoreAbout title="More About Me"></MoreAbout>
-      <Skills title="Skills"></Skills>
-      <Projects title="Projects"></Projects>
-      <ContactMe title={"Contact Me"}></ContactMe>
-      <Socials social_links={
-        {
-          "linkedin": "https://www.linkedin.com/in/logan-ankarberg-a6504b280/",
-          "github": "https://github.com/loganank"
-        }
-      }></Socials>
+      LUKE
     </div>
   );
 }
